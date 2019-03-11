252c7f22fc20fddc677ac3c27ffcdd19db249189
Nathaniel Wilcox <nathaniel.wilcox@gmail.com>
2019-03-11 15:22:35 -0600

    flip working

--- script.js
+++ script.js
@@ -10,19 +10,34 @@ var app = new Vue(
         rus: 'привет',
         ex_e: 'Bob said hi to Alice.',
         ex_r: 'Боб сказал Алисе привет.',
+        show_eng: false,
       },
       {
         eng: 'I',
         rus: 'я',
         ex_e: 'I am happy.',
         ex_r: 'Я счастлив(а).',
+        show_eng: false,
       },
       {
         eng: 'you',
         rus: 'вы',  
         ex_e: 'Are you happy?',
         ex_r: 'Вы счастливы?',
+        show_eng: false,
       }     
     ],
+    selectedWord: null,
   },
+
+  methods:
+  {
+    wordClicked: (word) =>
+    {
+      word.show_eng = !word.show_eng;
+      this.selectedWord = word;
+    }
+  }
+
+    
 });
